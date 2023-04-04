5a33a36e7525061cad86024346f61c02bb07c56c
Sachin <sachinbajaj0001@gmail.com>
2023-04-04 14:46:08 +0530

    props

---
+++ src/Cart.js
@@ -0,0 +1,52 @@
+import React from "react";
+import CartItem from './CartItem';
+
+class Cart extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'Watch',
+                    qty: 1,
+                    img: '',
+                    id: 1
+                },
+                {
+                    price: 9999,
+                    title: 'Phone',
+                    qty: 1,
+                    img: '',
+                    id: 2
+                },
+                {
+                    price: 49999,
+                    title: 'Laptop',
+                    qty: 1,
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+    }
+
+    render() {
+        // const arr = [1, 2, 3, 4, 5];
+        const { products } = this.state;
+        return (
+            <div className="cart">
+                {/* <CartItem qty={1} price={99}  title={"Watch"} img={''} /> */}
+                {products.map((product) => {
+                    return (
+                        <CartItem product={product} key={product.id} />
+                    ) 
+                })}
+            </div>
+        );
+    }
+
+}
+
+export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,23 +2,47 @@ import React from "react";
 
 class CartItem extends React.Component {
 
-    constructor(){
-        super();
-        this.state = {
-            price: 9999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-    }
-
     increaseQuantity = () => {
         // console.log("Test");
-        console.log('this.state', this.state);
+        // this.state.qty+=1;
+
+        // first method
+        // this.setState({
+        //     qty: this.state.qty+1
+        // });
+
+        //second method 
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty+1
+            }
+        }, () => {
+            console.log('this.state', this.state);
+        });
+        // console.log('this.state', this.state);
+    }
+
+    decreaseQuantity = () => {
+
+        const { qty } = this.state;
+
+        if(qty===0){
+            alert("Cannot be deleted");
+            return;
+        }
+
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty-1
+            }
+        }, () => {
+            console.log('this.state', this.state);
+        });
     }
 
     render() {
-        const{price, title, qty} = this.state;
+        console.log('this.props', this.props);
+        const{ price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -36,7 +60,7 @@ class CartItem extends React.Component {
                     <div className="cart-item-actions">
                         {/* + - Delete */}
                         <img className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" alt="Increase" onClick={this.increaseQuantity}/>
-                        <img className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" alt="Decrease" onClick={this.increaseQuantity}/>
+                        <img className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" alt="Decrease" onClick={this.decreaseQuantity}/>
                         <img className="action-icons" src="https://cdn-icons-png.flaticon.com/512/484/484611.png" alt="Delete" onClick={this.increaseQuantity}/>
                     </div>
                 </div>
