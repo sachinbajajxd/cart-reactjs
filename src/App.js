7a1042e912e14d8e231de87ceeddf01ea71986b0
Sachin <sachinbajaj0001@gmail.com>
2023-04-09 16:02:21 +0530

    Finished Cart Project

--- src/App.js
+++ src/App.js
@@ -2,48 +2,71 @@ import React from "react";
 // import CartItem from './CartItem';
 import Cart from './Cart';
 import Navbar from "./Navbar";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 class App extends React.Component {
 
     constructor(){
       super();
       this.state = {
-          products: [
-              {
-                  price: 999,
-                  title: 'Watch',
-                  qty: 1,
-                  img: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQDY3ref_VW_34FR+watch-49-titanium-ultra_VW_34FR_WF_CO+watch-face-49-alpine-ultra_VW_34FR_WF_CO_GEO_IN?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1660713659063%2C1660927566964%2C1661371890735',
-                  id: 1
-              },
-              {
-                  price: 9999,
-                  title: 'Phone',
-                  qty: 1,
-                  img: 'https://i0.wp.com/maplestore.in/wp-content/uploads/2021/09/r1433_Starlight_PDP_Image_Position-1B_Avail__en-IN.jpg?fit=2048%2C2048&ssl=1',
-                  id: 2
-              },
-              {
-                  price: 49999,
-                  title: 'Laptop',
-                  qty: 1,
-                  img: 'https://media.istockphoto.com/id/1157789866/photo/modern-computer-laptop-with-blank-screen-on-counter-barand-window-view.jpg?s=612x612&w=0&k=20&c=Idggc96ENAdz7R_ANTzvWpMTbaUcTV4JvJYKJHAD71A=',
-                  id: 3
-              }
-          ]
+          products: [],
+          loading: true
       }
-  }
+      this.db=firebase.firestore();
+    }
+
+    componentDidMount () {
+      
+      this.db
+        .collection('products')
+        .onSnapshot((snapshot) => {
+          // console.log(snapshot);
+
+          snapshot.docs.map((doc) => {
+            console.log(doc.data());
+            return "";
+          });
+
+          const products = snapshot.docs.map((doc) => {
+            const data=doc.data();
+            data["id"]=doc.id;
+            return data;
+          })
+
+          this.setState({
+            products: products,
+            loading: false
+          })
+
+        })
+
+    }
 
   handleIncreaseQuantity = (product) => {
       console.log('Increase qty.', product);
       const { products } = this.state;
       const index = products.indexOf(product);
-      products[index].qty += 1;
 
-      this.setState({
-          // products: products
-          products
-      })
+      const docRef=this.db.collection('products').doc(products[index].id);
+
+      docRef
+        .update({
+          qty: products[index].qty+1
+        })
+        .then(() => {
+          console.log("Updated successfully");
+        })
+        .catch((err) => {
+          console.log("Error in updating", err);
+        })
+
+      // products[index].qty += 1;
+
+      // this.setState({
+      //     // products: products
+      //     products
+      // })
   }
 
   handleDecreaseQuantity = (product) => {
@@ -54,22 +77,47 @@ class App extends React.Component {
           console.log("Cannot delete");
           return;
       }
-      products[index].qty -= 1;
 
-      this.setState({
-          // products: products
-          products
-      })
+      const docRef=this.db.collection('products').doc(products[index].id);
+
+      docRef
+        .update({
+          qty: products[index].qty-1
+        })
+        .then(() => {
+          console.log("Updated successfully");
+        })
+        .catch((err) => {
+          console.log("error", err);
+        })
+
+      // products[index].qty -= 1;
+
+      // this.setState({
+      //     // products: products
+      //     products
+      // })
   }
 
   handleDeleteProduct =(id) => {
       const {products} = this.state;
 
-      const items = products.filter((item) => item.id !== id);// it will return an updated array with updated product
+      const docRef=this.db.collection('products').doc(id);
 
-      this.setState({
-          products: items
-      })
+      docRef
+        .delete()
+        .then(() => {
+          console.log("Deleted successfully");
+        })
+        .catch((e) => {
+          console.log("Error in deleting", e);
+        })
+
+      // const items = products.filter((item) => item.id !== id);// it will return an updated array with updated product
+
+      // this.setState({
+      //     products: items
+      // })
 
   }
 
@@ -97,21 +145,40 @@ class App extends React.Component {
     return total;
   }
 
+  addProduct = () => {
+    this.db
+      .collection('products')
+      .add({
+        img:'',
+        price:35000,
+        title:'Washing Machine',
+        qty: 1
+      })
+      .then((docRef) => {
+        console.log("Product has been added to the DB", docRef)
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      })
+  }
+
   render() {
 
-    const { products } = this.state;
+    const { products, loading } = this.state;
 
     return (
       <div className="App">
         <Navbar
           count={this.getCartCount()}
         />
+        {/* <button onClick={this.addProduct}>Add Product</button> */}
         <Cart 
           products = {products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1>Loading...</h1>}
         <div style={ {fontSize: 20, padding: 10} }>TOTAL : {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCICQp_ewKvW_j-eWUpAidW6W52fZyyrNI",
+  authDomain: "cart-c899b.firebaseapp.com",
+  projectId: "cart-c899b",
+  storageBucket: "cart-c899b.appspot.com",
+  messagingSenderId: "324197861174",
+  appId: "1:324197861174:web:51cd4b9a850104205a9597"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
